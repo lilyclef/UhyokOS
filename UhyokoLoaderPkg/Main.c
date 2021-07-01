f12d6eb196ae45152176a6f9be977372229110d4
Yuri Yamamoto <lilyclef@gmail.com>
2021-07-01 19:34:16 +0900

    メモリの状態をUFIで習得するのに必要な関数の宣言

--- UhyokoLoaderPkg/Main.c
+++ UhyokoLoaderPkg/Main.c
@@ -1,6 +1,32 @@
 #include  <Uefi.h>
 #include  <Library/UefiLib.h>
 
+/* メモリマップという構造体を作り、メモリの状態を保管する */
+struct MemoryMap {
+  UINTN buffer_size;
+  VOID* buffer;
+  UINTN map_size;
+  UINTN map_key;
+  UINTN descriptor_size;
+  UINT32 descriptor_version;
+};
+
+/* メモリマップを習得
+   UEFIは大きく分けてブートサービス(gBS)とラインタイムサービス(gRT)に分かれる
+   メモリ管理はブートサービスが管理する
+*/
+EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
+  return EFI_SUCCESS;
+}
+
+/* メモリマップのファイル保存
+   EFI_FILE_PROTOCOL：UEFIではFATファイルシステムを扱える
+*/
+EFI_STATUS SaveMemoryMap(struct MemoryMap* map, EFI_FILE_PROTOCOL* file) {
+  return EFI_SUCCESS;
+}
+
+/* UEFI:OSとファームウェアを仲介するソフトウェアインターフェース */
 EFI_STATUS EFIAPI UefiMain(
     EFI_HANDLE image_handle,
     EFI_SYSTEM_TABLE *system_table) {
