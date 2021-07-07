aff365675f2a9347d6f68675f3d23adb6bafd996
lilybrevec <allabrevec@gmail.com>
2021-07-07 18:09:48 +0900

    円を描く・C++継承クラス分けを学ぶ・簡単便利な配置new

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,13 +1,101 @@
 #include <cstdint>
+#include <cstddef>
+
+#include "frame_buffer_config.hpp"
+
+struct PixelColor {
+  uint8_t r, g, b;
+};
+
+// (:3 PixelWriter begin
+class PixelWriter {
+public:
+  PixelWriter(const FrameBufferConfig& config) : config_{config} {
+  }
+  virtual ~PixelWriter() = default;
+  // プロトタイプ宣言の後ろに=0 : 純粋仮想関数 インターフェース的な
+  virtual void Write(int x, int y, const PixelColor& c) = 0;
+protected:
+  uint8_t* PixelAt(int x, int y) {
+    return config_.frame_buffer + 4 * (config_.pixels_per_scan_line * y + x);
+  }
+private:
+  const FrameBufferConfig& config_;
+};
+// (:3 PixelWriterClass end
+
+
+// インターフェースっぽく書かれたクラスを継承する子クラス=>使えるように。
+// RGB表記
+class RGBResv8BitPerColorPixelWriter : public PixelWriter {
+public:
+  // 親クラスのコンストラクタを子のコンストラクタとして扱える。
+  using PixelWriter::PixelWriter;
+  virtual void Write(int x, int y, const PixelColor& c) override {
+    auto p = PixelAt(x, y);
+    p[0] = c.r;
+    p[1] = c.g;
+    p[2] = c.b;
+  }
+};
+
+// BGR表記
+class BGRResv8BitPerColorPixelWriter : public PixelWriter {
+public:
+  using PixelWriter::PixelWriter;
+  virtual void Write(int x, int y, const PixelColor& c) override {
+    auto p = PixelAt(x, y);
+    p[0] = c.b;
+    p[1] = c.g;
+    p[2] = c.r;
+  }
+};
+
+// (:3 配置newを設定するための準備 begin
+void* operator new(size_t size, void* buf) {
+  return buf;
+}
+
+void operator delete(void* obj) noexcept {
+}
+// (:3 配置newを設定するための準備 end
+
+// (:3 グローバル変数 begin
+char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
+PixelWriter* pixel_writer;
+// (:3 グローバル変数 end
+
 /*
   KernelMain()がブートローダから呼び出される
   エントリポイントと呼ぶ
 */
-extern "C" void KernelMain(uint64_t frame_buffer_base,
-                           uint64_t frame_buffer_size) {
-  uint8_t* frame_buffer = reinterpret_cast<uint8_t*> (frame_buffer_base);
-  for (uint64_t i = 0; i < frame_buffer_size; ++i) {
-    frame_buffer[i] = i % 256;
+extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
+  switch (frame_buffer_config.pixel_format) {
+  case kPixelRGBResv8BitPerColor:
+    // このnewは配置newというもの
+    // OSがメモリ管理できない状況なので、newは使えないが、メモリ領域上にインスタンスを作成して、コンストラクタを呼び出す。
+    pixel_writer = new(pixel_writer_buf)
+      RGBResv8BitPerColorPixelWriter{frame_buffer_config};
+    break;
+  case kPixelBGRResv8BitPerColor:
+    pixel_writer = new(pixel_writer_buf)
+      BGRResv8BitPerColorPixelWriter{frame_buffer_config};
+    break;
+  }
+
+  // 背景を丁寧に塗る
+  for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {
+    for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y) {
+      pixel_writer->Write(x, y, {143, 204, 232});
+    }
+  }
+  // 円を丁寧に書く
+  for (int x = 0; x < 400; ++x) {
+    for (int y = 0; y < 400; ++y) {
+      if ((x - 200)*(x - 200) + (y - 200)*(y - 200) <= 40000) {
+        pixel_writer->Write(x, y, {234, 145, 152});
+      }
+    }
   }
   while (1) __asm__("hlt");
 }
