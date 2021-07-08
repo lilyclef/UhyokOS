8a52c23c0d28010f2c0b48dbadb9ebb322a01a97
lilybrevec <allabrevec@gmail.com>
2021-07-08 13:16:59 +0900

    fontモジュール分け

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,6 +1,7 @@
 #include <cstdint>
 #include <cstddef>
 #include "graphics.hpp"
+#include "font.hpp"
 
 // (:3 配置newを設定するための準備 begin
 void* operator new(size_t size, void* buf) {
@@ -16,37 +17,6 @@ char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
 // (:3 グローバル変数 end
 
-const uint8_t kFontA[16] = {
-  0b00000000, //
-  0b00011000, //    **
-  0b00011000, //    **
-  0b00011000, //    **
-  0b00011000, //    **
-  0b00100100, //   *  *
-  0b00100100, //   *  *
-  0b00100100, //   *  *
-  0b00100100, //   *  *
-  0b01111110, //  ******
-  0b01000010, //  *    *
-  0b01000010, //  *    *
-  0b01000010, //  *    *
-  0b11100111, // ***  ***
-  0b00000000, //
-  0b00000000, //
-};
-
-void WriteAscii(PixelWriter& writer, int x, int y, char c, const PixelColor& color) {
-  if (c != 'A') {
-    return;
-  }
-  for (int dy = 0; dy < 16; ++dy) {
-    for (int dx = 0; dx < 8; ++dx) {
-      if ((kFontA[dy] << dx) & 0x80u) {
-        writer.Write(x + dx, y + dy, color);
-      }
-    }
-  }
-}
 
 /*
   KernelMain()がブートローダから呼び出される
