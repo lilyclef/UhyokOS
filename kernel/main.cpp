4e23bd82f7ccacc14c2f1120c0ed8e3ceb7c5aec
Yuri Yamamoto <lilyclef@gmail.com>
2021-07-09 15:46:38 +0900

    デスクトップ画面

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -17,9 +17,57 @@ void operator delete(void* obj) noexcept {
 // (:3 配置newを設定するための準備 end
 
 // (:3 グローバル変数 begin
+const PixelColor kDesktopBGColor{181, 234, 234};
+const PixelColor kDesktopFGColor{142, 127, 127};
+
+const int kMouseCursorWidth = 15;
+const int kMouseCursorHeight = 24;
+const char mouse_cursor_shape[kMouseCursorHeight][kMouseCursorWidth + 1] = {
+  "@              ",
+  "@@             ",
+  "@@@            ",
+  "@@@@           ",
+  "@@.@@          ",
+  "@@..@@         ",
+  "@@...@@        ",
+  "@@....@@       ",
+  "@@.....@@      ",
+  "@@......@@     ",
+  "@@.......@@    ",
+  "@@........@@   ",
+  "@@.........@@  ",
+  "@@.......@@@@@ ",
+  "@@......@@@@@@@",
+  "@@......@      ",
+  "@@..@@..@      ",
+  "@@.@@ @..@     ",
+  "@@@@   @..@    ",
+  "@@@    @..@    ",
+  "@@      @..@   ",
+  "@       @..@   ",
+  "         @..@  ",
+  "         @@@@  ",
+};
+
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
+char console_buf[sizeof(Console)];
+Console* console;
 // (:3 グローバル変数 end
+
+
+int printk(const char* format, ...) {
+  va_list ap;
+  int result;
+  char s[1024];
+
+  va_start(ap, format);
+  result = vsprintf(s, format, ap);
+  va_end(ap);
+
+  console->PutString(s);
+  return result;
+}
 
 
 /*
@@ -40,8 +88,26 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
     break;
   }
 
+  const int kFrameWidth = frame_buffer_config.horizontal_resolution;
+  const int kFrameHeight = frame_buffer_config.vertical_resolution;
+
+  FillRectangle(*pixel_writer,
+                {0, 0},
+                {kFrameWidth, kFrameHeight - 50},
+                kDesktopBGColor);
+  FillRectangle(*pixel_writer,
+                {0, kFrameHeight - 50},
+                {kFrameWidth, 50},
+                {243, 139, 160});
+  FillRectangle(*pixel_writer,
+                {0, kFrameHeight - 50},
+                {kFrameWidth / 5, 50},
+                {237, 246, 229});
+  console = new(console_buf) Console{
+    *pixel_writer, kDesktopFGColor, kDesktopBGColor
+  };
   // 背景を丁寧に塗る
-  for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {
+  /*for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {
     for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y) {
       pixel_writer->Write(x, y, {143, 204, 232});
     }
@@ -53,22 +119,23 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
         pixel_writer->Write(x, y, {234, 145, 152});
       }
     }
-  }
-  char buf[128];
-  sprintf(buf, "1 + 2 = %d", 1 + 2);
-  WriteString(*pixel_writer, 200, 82, buf, {0, 0, 0});
+    }*/
 
-  WriteString(*pixel_writer, 100, 100, "(:3", {0, 0, 0});
-  int i = 0;
-  for (char c = '!'; c <= '~'; ++c, ++i) {
-    WriteAscii(*pixel_writer, 8 * i, 50, c, {0, 0, 0});
-  }
-  Console console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
 
   char buf2[128];
   for (int i = 0; i < 27; ++i) {
     sprintf(buf2, "line %d\n", i);
-    console.PutString(buf2);
+    console->PutString(buf2);
+  }
+  WriteString(*pixel_writer, 100, 100, "(:3", {0, 0, 0});
+  for (int dy = 0; dy < kMouseCursorHeight; ++dy) {
+    for (int dx = 0; dx < kMouseCursorWidth; ++dx) {
+      if (mouse_cursor_shape[dy][dx] == '@') {
+        pixel_writer->Write(200 + dx, 100 + dy, {255, 255, 255});
+      } else if (mouse_cursor_shape[dy][dx] == '.') {
+        pixel_writer->Write(200 + dx, 100 + dy, kDesktopFGColor);
+      }
+    }
   }
   while (1) __asm__("hlt");
 }
