6bb9e727506eeea29806aae95dda3ac137d947ce
Yuri Yamamoto <lilyclef@gmail.com>
2022-03-21 21:57:44 +0900

    #9 Acceleration by FrameBuffer/ShadowBuffer

--- kernel/mouse.cpp
+++ kernel/mouse.cpp
@@ -50,11 +50,11 @@ void DrawMouseCursor(PixelWriter* pixel_writer, Vector2D<int> position) {
   for (int dy = 0; dy < kMouseCursorHeight; ++dy) {
     for (int dx = 0; dx < kMouseCursorWidth; ++dx) {
       if (mouse_cursor_shape[dy][dx] == '@') {
-        pixel_writer->Write(position.x + dx, position.y + dy, {142, 127, 127});
+        pixel_writer->Write(position + Vector2D<int>{dx, dy}, {142, 127, 127});
       } else if (mouse_cursor_shape[dy][dx] == '.') {
-        pixel_writer->Write(position.x + dx, position.y + dy, {255, 255, 255});
+        pixel_writer->Write(position + Vector2D<int>{dx, dy}, {255, 255, 255});
       } else {
-        pixel_writer->Write(position.x + dx, position.y + dy, kMouseTransparentColor);
+        pixel_writer->Write(position + Vector2D<int>{dx, dy}, kMouseTransparentColor);
       }
 
     }
