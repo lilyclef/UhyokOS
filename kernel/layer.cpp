78227eaf171ce977c3cb2f5ad86b1aabbc54e4fc
Yuri Yamamoto <lilyclef@gmail.com>
2022-03-23 02:32:08 +0900

    #10 Enable to move app window

--- kernel/layer.cpp
+++ kernel/layer.cpp
@@ -28,6 +28,16 @@ Vector2D<int> Layer::GetPosition() const {
   return pos_;
 }
 
+// [10.32]
+Layer& Layer::SetDraggable(bool draggable) {
+  draggable_ = draggable;
+  return *this;
+}
+
+bool Layer::IsDraggable() const {
+  return draggable_;
+}
+
 // #@@range_begin(layer_move)
 Layer& Layer::Move(Vector2D<int> pos) {
   pos_ = pos;
@@ -142,6 +152,29 @@ void LayerManager::Hide(unsigned int id) {
   }
 }
 // #@@range_end(layermgr_hide)
+
+Layer* LayerManager::FindLayerByPosition(Vector2D<int> pos, unsigned int exclude_id) const {
+  auto pred = [pos, exclude_id](Layer* layer) {
+    // Avoid find mouse pointer as the top of layers
+    if (layer->ID() == exclude_id) {
+      return false;
+    }
+    const auto& win = layer->GetWindow();
+    if (!win) {
+      return false;
+    }
+    const auto win_pos = layer->GetPosition();
+    const auto win_end_pos = win_pos + win->Size();
+    return win_pos.x <= pos.x && pos.x < win_end_pos.x &&
+           win_pos.y <= pos.y && pos.y < win_end_pos.y;
+  };
+  auto it = std::find_if(layer_stack_.rbegin(), layer_stack_.rend(), pred);
+  if (it == layer_stack_.rend()) {
+    return nullptr;
+  }
+  return *it;
+}
+
 
 // #@@range_begin(layermgr_findlayer)
 Layer* LayerManager::FindLayer(unsigned int id) {
